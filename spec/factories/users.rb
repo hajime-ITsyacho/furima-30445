ba1700a29cb418a6a19b087a54953e3031e0fb5e
hajime-ITsyacho <hyokei614@gmail.com>
2020-10-21 14:36:01 +0900

    ユーザー管理機能の実装修正２

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.name }
     email { Faker::Internet.free_email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { 1a2a3a4a5a }
     password_confirmation { password }
     first_name { 'あ' }
     last_name { 'い' }
